b2573d07675db96edfed9bf11488f5d87242767f
Yeo Jia Hao <jiahao.yeo@digipen.edu>
2024-10-03 10:52:30 +0800

    Added lib

--- src/SageJSON.cpp
+++ src/SageJSON.cpp
@@ -1,10 +1,7 @@
-#include "SageJSON.hpp"
+#include "../include/SageJSON.hpp"
 #include <fstream>
 #include<string>
-#include <crtdbg.h>
 #include <stack>
-#define _CRTDBG_MAP_ALLOC
-#define new new(_NORMAL_BLOCK, __FILE__, __LINE__)
 namespace SageJSON::Lexer
 {
 	void Token::print()
